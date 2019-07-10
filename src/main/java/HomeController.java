158233b2e1155eadf8a6ca5fdf2e86f697f1b621
togoboy8 <cedrichillah@gmail.com>
2019-07-10 16:58:41 -0400

    second commit

--- src/main/java/HomeController.java
+++ src/main/java/HomeController.java
@@ -1,11 +1,54 @@
-package com.example.demo;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 
 public class HomeController {
-    @RequestMapping("/test")
-    public String homePage(){
+    @RequestMapping("/")
+    public String indexPage() {
         return "index";
     }
+
+
+    @RequestMapping("/Home")
+    public String homePage() {
+        return "Home";
+    }
+
+
+    @RequestMapping("/Blending")
+    public String blendingPage() {
+        return "Blending";
+    }
+
+
+    @RequestMapping("/FrozenStrawberryGrapeJuice")
+    public String frozenStrawberryGrapeJuicePage() {
+        return "FrozenStrawberryGrapeJuice";
+    }
+
+
+    @RequestMapping("/FrozenWhiskySour")
+    public String frozenWhiskySourPage() {
+        return "FrozenWhiskySour";
+    }
+
+
+    @RequestMapping("/MilkShake")
+    public String milkShakePage() {
+        return "MilkShake";
+    }
+
+
+    @RequestMapping("/TripleBerrySmoothie")
+    public String tripleBerrySmoothiePage() {
+        return "TripleBerrySmoothie";
+    }
+
+
+    @RequestMapping("/WholeFruitMargarita")
+    public String wholeFruitMargaritaPage() {
+        return "WholeFruitMargarita";
+    }
 }
+
